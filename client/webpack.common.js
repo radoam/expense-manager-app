54c0a1e8ef95a929e49f015784760dafd99de5a5
Radoslaw Luczak <rluczak@egnyte.com>
2022-05-26 10:51:52 +0200

    Add rules for webpack configuration

--- client/webpack.common.js
+++ client/webpack.common.js
@@ -8,6 +8,32 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
     },
+    module: {
+        rules: [
+            {
+                test: /\.?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                    },
+                },
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(png|jp(e*)g|svg|gif)$/,
+                use: ['file-loader'],
+            },
+            {
+                test: /\.svg$/,
+                use: ['@svgr/webpack'],
+            },
+        ],
+    },
     plugins: [
         new HtmlWebpackPlugin({
             title: "Expense manager app",
