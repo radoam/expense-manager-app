87bec6a57578c7d64cda064183a4e532277c10ce
Radoslaw Luczak <rluczak@egnyte.com>
2022-05-30 10:49:48 +0200

    Added tailwind css

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const App = () => {
   const name = 'Rado';
 
-  return <h1 className="helloWorld">Hello world {name}</h1>;
+  return <h1 className="text-3xl font-bold">Hello world! {name}</h1>;
 };
 
 export default App;
--- client/webpack.common.js
+++ client/webpack.common.js
@@ -4,55 +4,55 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    entry: path.join(__dirname, 'src', 'index.tsx'),
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+  entry: path.join(__dirname, 'src', 'index.tsx'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    port: 3000
+  },
+  resolve: {
+    alias: {
+      src: path.join(__dirname, 'src')
     },
-    devServer: {
-        port: 3000,
-    },
-    resolve: {
-        alias: {
-            src: path.join(__dirname, 'src'),
-        },
-        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(jsx|js|tsx|ts)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react', "@babel/preset-typescript"],
-                    },
-                },
-            },
-            {
-                test: /\.(sa|sc|c)ss$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-            },
-            {
-                test: /\.(png|jp(e*)g|svg|gif)$/,
-                use: ['file-loader'],
-            },
-            {
-                test: /\.svg$/,
-                use: ['@svgr/webpack'],
-            },
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: "Expense manager app",
-            favicon: path.join(__dirname, 'public', 'favicon.svg'),
-            template: path.join(__dirname, 'public', 'index.html'),
-        }),
-        new MiniCssExtractPlugin({
-            filename: 'main.css',
-            chunkFilename: 'main.[id].css',
-        }),
-    ],
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jsx|js|tsx|ts)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript']
+          }
+        }
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader', 'postcss-loader']
+      },
+      {
+        test: /\.(png|jp(e*)g|svg|gif)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Expense manager app',
+      favicon: path.join(__dirname, 'public', 'favicon.svg'),
+      template: path.join(__dirname, 'public', 'index.html')
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'main.css',
+      chunkFilename: 'main.[id].css'
+    })
+  ]
 };
