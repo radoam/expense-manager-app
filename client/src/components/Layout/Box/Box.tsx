5c262971feec8151368ad8fad14e526dbe14433a
Radoslaw Luczak <rluczak@egnyte.com>
2022-07-25 09:27:08 +0200

    Clear project

--- client/src/components/Layout/Box/Box.tsx
+++
@@ -1,9 +0,0 @@
-import { FC, ReactNode } from 'react';
-
-import { BoxStyled } from './Box.styles';
-
-interface BoxProps {
-  children: ReactNode;
-}
-
-export const Box: FC<BoxProps> = ({ children }) => <BoxStyled>{children}</BoxStyled>;
